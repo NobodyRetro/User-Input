53db1c19eed126dc93b46d7ba205fe4188ac7346
NobodyRetro <59867447+NobodyRetro@users.noreply.github.com>
2023-05-31 19:31:50 -0700

    Add files via upload (#1)

--- UserInput/Program.cs
+++ UserInput/Program.cs
@@ -1,4 +1,7 @@
 ﻿Console.WriteLine("What is your name?");
 var UserName = Console.ReadLine();
 Console.WriteLine("Hello " + UserName);
+Console.WriteLine("How old are you?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("You are {0} years old", age);
 Console.ReadKey();
